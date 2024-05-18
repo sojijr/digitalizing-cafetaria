025f3198b07989c26715dd5e36d98c80bc5612e2
Ayomikun <ayoadeosun10@gmail.com>
2024-05-18 16:53:05 +0100

    redesigned index page and added mobile responsiveness

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-const passwordInput = document.getElementById("Password");
+const passwordInput = document.getElementById("password");
 const passwordToggle = document.getElementById("password-toggle");
 
 passwordToggle.addEventListener("click", () => {
+  const type =
+    passwordInput.getAttribute("type") === "password" ? "text" : "password";
+  passwordInput.setAttribute("type", type);
+
   if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    passwordToggle.classList.remove("fa-eye");
-    passwordToggle.classList.add("fa-eye-slash");
-  } else {
-    passwordInput.type = "password";
     passwordToggle.classList.remove("fa-eye-slash");
     passwordToggle.classList.add("fa-eye");
+  } else {
+    passwordToggle.classList.remove("fa-eye");
+    passwordToggle.classList.add("fa-eye-slash");
   }
 });
