bcf39204c3b93c90904181d4ae1e41be81217ba5
Oluwadamilola Soji-Oderinde <126824652+sojijr@users.noreply.github.com>
2024-06-21 18:13:21 +0100

    feat: add favicon

--- dashboard.php
+++ dashboard.php
@@ -39,6 +39,7 @@ if($result) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
--- index.php
+++ index.php
@@ -25,6 +25,7 @@ if (isset($_POST['login'])) {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
